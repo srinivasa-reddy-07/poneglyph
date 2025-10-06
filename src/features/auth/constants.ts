3e255a7d8cde11326f899698de59445328a5c4db
srinivasa-reddy-07 <srinivasareddykapa18@gmail.com>
2025-10-06 20:41:09 +0530

    Connected appwrite

---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "poneglyph-session"
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,24 +1,46 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
+import { ID } from "node-appwrite";
+import { setCookie } from "hono/cookie";
+
 import { loginSchema, registerSchema } from "../schemas";
+import { createAdminClient } from "@/lib/appwrite";
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
-  .post(
-    "/login",
-     zValidator("json", loginSchema), 
-     async (c) => {
+  .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
-    console.log({ email, password });
-    return c.json({ email, password });
+
+    const { account } = await createAdminClient();
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ success: true });
   })
-  .post(
-    "/register", 
-    zValidator("json", registerSchema), 
-    async (c) => {
+  .post("/register", zValidator("json", registerSchema), async (c) => {
     const { name, email, password } = c.req.valid("json");
-    console.log({ name, email, password });
-    return c.json({ name, email, password });
+
+    const { account } = await createAdminClient();
+    await account.create(ID.unique(), email, password, name);
+
+    const session = await account.createEmailPasswordSession(email, password);
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ success: true });
   });
 
 export default app;
